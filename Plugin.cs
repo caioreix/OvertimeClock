8bb50fb6263b79c41a7841fb89d7c8e45865bf20
Caio Almeida <caioreix@outlook.com>
2024-06-16 19:26:03 -0300

    Add mod template

---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using SRDebugger.Services.Implementation;
+
+namespace PluginName;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+
+public class Plugin : BasePlugin {
+    private static Harmony harmony;
+    public override void Load() {
+        Settings.Config.Load(Config, Log, "Client");
+
+        harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        Utils.Logger.Log.Trace("Patching harmony");
+        harmony.PatchAll();
+
+        Utils.Logger.Log.Info($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
